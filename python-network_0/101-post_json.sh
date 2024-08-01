7ab5304cfea8679f66515eab353a2c4e02db73f8
EdwinZed <e.bayingana@alustudent.com>
2024-08-01 14:22:13 +0200

    fix the py network files

--- python-network_0/101-post_json.sh
+++ python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Bash script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response
-curl -s "$1" -d "@$2" -X POST -H "Content-Type: application/json"
+# This script sends a JSON POST request to a URL passed as the first argument, and displays the body of the response. Also send a POST request with the contents of a file, passed with the filename as the second argument of the script, in the body of the request.
+curl -s "$1" -X POST -H "Content-Type: application/json" -d "$(cat "$2")"
